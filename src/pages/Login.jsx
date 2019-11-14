2f752695fbd09a487658ca753a4d075466ee6e1d
Andre <andremerchant@gmail.com>
2019-11-14 15:18:07 -0600

    login con redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 
 import '../assets/styles/Login.scss'
 
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => {
+const Login = props => {
     const [ form, setValues ] = useState({
         email: '',
     });
@@ -20,7 +22,8 @@ const Login = () => {
 
     const handleSubmit = event => {
         event.preventDefault();
-        console.log(form);
+        props.loginRequest(form);
+        props.history.push('/');
     }
     
     return(
@@ -72,5 +75,8 @@ const Login = () => {
     )
 }
 
+const mapDispatchToProps = {
+    loginRequest,
+}
 
-export default Login;
+export default connect(null,mapDispatchToProps)(Login);
